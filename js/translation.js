daabd8ba7bcaa9eb049581843791e32dd1d4eb9d
EdileneLuis04 <eddysantosluis@gmail.com>
2024-12-18 11:36:42 +0100

    translations

---
+++ js/translation.js
@@ -0,0 +1,250 @@
+const translations = {
+  pt: {
+    title: "Inovatec, Mais que inovação",
+    slogan: "Mais que Tecnologia, Inovação!",
+    description:
+      "A INOVATEC é uma empresa focada em fornecer soluções tecnológicas avançadas para a indústria petrolífera e outros setores estratégicos.",
+    descriptionContent:
+      "Desenvolvemos sistemas inovadores e oferecemos a comercialização e distribuição de produtos especializados, além de serviços de consultoria e outsourcing que aumentam a eficiência e competitividade dos nossos clientes. Nosso compromisso com a inovação, excelência, colaboração, sustentabilidade, foco no cliente e ética nos impulsiona a ser a parceira tecnológica de confiança, entregando soluções personalizadas e de alta qualidade.",
+    contact: "Contacte-nos",
+    fullName: "Nome completo",
+    email: "Endereço de email",
+    message: "Mensagem",
+    submit: "Contacte-me",
+    home: "Início",
+    about: "Sobre Nós",
+    offer1:
+      "01. Fornecemos equipamentos mecânicos de alta qualidade para garantir eficiência e produtividade em seus projetos.",
+    offer1Content: "Procurement e Suprimentos Mecânicos",
+    offer2:
+      "02. Oferecemos suprimentos elétricos e de telecomunicações que atendem às demandas tecnológicas do setor.",
+    offer2Content: "Soluções em Elétrica e Telecom",
+    offer3:
+      "03. Garantimos segurança e confiabilidade com fornecimento de EPI’s, instrumentos e equipamentos essenciais.",
+    offer3Content: "Equipamentos de Proteção e Instrumentos",
+    services: "Serviços/Produtos",
+    valueProposal: "Proposta de Valor",
+    clients: "Nossos Clientes",
+    contacts: "Contactos",
+    mission: "Missão",
+    missionContent:
+      "Fornecer soluções tecnológicas de ponta, desenvolvendo sistemas inovadores e a comercialização e distribuição de produtos específicos que atendam às exigências da indústria petrolífera, enquanto oferecemos serviços de consultoria e outsourcing especializados que potencializem a eficiência e a competitividade dos nossos clientes.",
+    vision: "Visão",
+    visionContent:
+      "Ser reconhecida como a parceira tecnológica de referência na indústria petrolífera e em outros sectores estratégicos, destacando-se pela inovação, confiabilidade e pela capacidade de entregar resultados que superem as expectativas dos nossos clientes.",
+    values: "Valores",
+    values1:
+      "Inovação: Buscamos constantemente novas tecnologias e abordagens para criar soluções eficientes e diferenciadas.",
+    values2:
+      "Compromisso com a Excelência: Entregamos produtos e serviços que excedem os padrões de qualidade e confiabilidade, essenciais para indústrias críticas como a petrolífera.",
+    values3:
+      "Colaboração: Construímos relações sólidas com clientes, parceiros e nossa equipa, garantindo sucesso mútuo.",
+    values4:
+      "Sustentabilidade: Promovemos práticas tecnológicas que respeitam o meio ambiente e ajudam a construir um futuro sustentável.",
+    values5:
+      "Foco no Cliente: Compreendemos as necessidades de cada cliente e oferecemos soluções personalizadas que agreguem valor real.",
+    values6:
+      "Ética e Integridade: Conduzimos todas as nossas operações com transparência, responsabilidade e respeito.",
+    newsletter: "Newsletter",
+    subscribe: "Inscrever-se",
+    electricalConsumables: "Consumíveis Elétricos",
+    electricalLighting: "Iluminação Elétrica",
+    electricalSafety: "Segurança Elétrica",
+    surgeArrestors: "Dispositivos de Supressão de Sobretensão",
+    telecommunications: "Telecomunicações",
+    instrumentation: "Instrumentação",
+    mechanicalTools: "Ferramentas Mecânicas",
+    fasteners: "Fixadores",
+    materialHandling: "Manuseio de Materiais",
+    hoseTubingAndFitting: "Mangueiras, Tubos e Conexões",
+    standardPPE: "PPE Padrão",
+    specializedPPE: " PPE Especializado",
+    hsePersonnel: "Pessoal de HSE",
+    projectManager: "Gerente de Projetos",
+    projectControlSpecialist: "Especialista em Controle de Projetos",
+    logisticsCoordinator: "Coordenador de Logística",
+    qualityControlSpecialist: "Especialista em Controle de Qualidade",
+    itAndTelecomEngineers: "Engenheiros de TI e Telecomunicações",
+    mechanicalEngineers: "Engenheiros Mecânicos",
+    mechanicalFitters: "Montadores Mecânicos",
+    welders: "Soldadores",
+    electricalEngineers: "Engenheiros Elétricos",
+    instrumentationEngineers: "Engenheiros de Instrumentação",
+    maintenanceEngineers: "Engenheiros de Manutenção",
+    products: "Produtos",
+    specializedPPE_2: "EPI Especializado",
+    ppe: "EPI",
+    valueproposition: "Proposta de valor",
+
+    valueProposition1:
+      "A INOVATEC oferece Procurement e Suprimentos Altamente Personalizados e de Nível Internacional, com Entregas Pontuais de Equipamentos Especializados e Pessoal em Angola.",
+    valueProposition2:
+      "A INOVATEC conta com engenheiros qualificados, com vasta experiência em sourcing global e um entendimento claro dos mecanismos locais de importação.",
+    valueProposition3:
+      "A INOVATEC possui um banco de dados rico de pessoal qualificado (nacional e internacional), ao qual podemos recorrer para atender às necessidades dos nossos clientes.",
+    valueProposition4:
+      "A INOVATEC tem experiência com outros clientes locais em interpretar os requisitos dos clientes por meio de seus objetivos estabelecidos.",
+    valueProposition5:
+      "Colaboramos com grandes fornecedores de equipamentos especializados nos mercados da África do Sul, Europa, Oriente Médio e Ásia.",
+    valueProposition6:
+      "Os prazos de entrega são melhor gerenciados pela nossa capacidade de estocagem.",
+    valueProposition7:
+      "Nossa base de parceiros nos permite equilibrar a eficácia de custos e os prazos de entrega, trabalhando de perto com parceiros confiáveis.",
+    valueProposition8:
+      "Também garantimos a qualidade e a autenticidade dos produtos, trabalhando apenas com fornecedores certificados de equipamentos especializados.",
+    valueProposition9:
+      "Contamos com uma equipe altamente qualificada para propor soluções de engenharia quando necessário.",
+    valuePropositionEnd:
+      "Temos parceiros globais especialistas que oferecem marcas alternativas quando possível.",
+    hoseTubingAndFitting_2: "Mangueiras, Tubos e Conexões",
+    materialHandling_2: "Manuseio de Materiais",
+    fastener: "Fixador",
+    mechanicalTools_2: "Ferramentas Mecânicas",
+    instrumentation_2: "Instrumentação",
+    telecommunication_2: "Telecomunicação",
+    surgeArrestors_2: "Supressores de picos",
+    ourclients: "Nossos Clientes",
+  },
+  en: {
+    title: "Inovatec, More than innovation",
+    slogan: "More than Technology, Innovation!",
+    description:
+      "INOVATEC is a company focused on providing advanced technological solutions for the oil industry and other strategic sectors.",
+    descriptionContent:
+      "We develop innovative systems and offer the commercialization and distribution of specialized products, as well as consulting and outsourcing services that enhance our clients' efficiency and competitiveness. Our commitment to innovation, excellence, collaboration, sustainability, customer focus, and ethics drives us to be the trusted technological partner, delivering high-quality and personalized solutions.",
+    contact: "Contact Us",
+    fullName: "Full Name",
+    email: "Email Address",
+    message: "Message",
+    submit: "Contact Me",
+    home: "Home",
+    about: "About Us",
+    offer1:
+      "01. We supply high-quality mechanical equipment to ensure efficiency and productivity in your projects.",
+    offer1Content: "Mechanical Procurement and Supplies",
+    offer2:
+      "02. We offer electrical and telecommunications supplies that meet the sector's technological demands.",
+    offer2Content: "Electrical and Telecom Solutions",
+    offer3:
+      "03. We guarantee safety and reliability with the supply of PPE, instruments, and essential equipment.",
+    offer3Content: "Protection Equipment and Instruments",
+    services: "Services/Products",
+    valueProposal: "Value Proposition",
+    clients: "Our Clients",
+    contacts: "Contacts",
+    mission: "Mission",
+    missionContent:
+      "To provide cutting-edge technological solutions by developing innovative systems and the commercialization and distribution of specific products that meet the demands of the oil industry while offering specialized consulting and outsourcing services to enhance our clients' efficiency and competitiveness.",
+    vision: "Vision",
+    visionContent:
+      "To be recognized as the leading technological partner in the oil industry and other strategic sectors, standing out for innovation, reliability, and the ability to deliver results that exceed our clients' expectations.",
+    values: "Values",
+    values1:
+      "Innovation: Constantly seeking new technologies and approaches to create efficient and differentiated solutions.",
+    values2:
+      "Commitment to Excellence: Delivering products and services that exceed quality and reliability standards, essential for critical industries like the oil sector.",
+    values3:
+      "Collaboration: Building strong relationships with clients, partners, and our team, ensuring mutual success.",
+    values4:
+      "Sustainability: Promoting technological practices that respect the environment and help build a sustainable future.",
+    values5:
+      "Customer Focus: Understanding each client's needs and providing customized solutions that add real value.",
+    values6:
+      "Ethics and Integrity: Conducting all our operations with transparency, responsibility, and respect.",
+    newsletter: "Newsletter",
+    subscribe: "Subscribe",
+    electricalConsumables: "Electrical Consumables",
+    electricalLighting: "Electrical Lighting",
+    electricalSafety: "Electrical Safety",
+    surgeArrestors: "Surge Arrestors",
+    telecommunications: "Telecommunications",
+    instrumentation: "Instrumentation",
+    mechanicalTools: "Mechanical Tools",
+    fasteners: "Fasteners",
+    materialHandling: "Material Handling",
+    hoseTubingAndFitting: "Hose Tubing and Fitting",
+    standardPPE: "Standard PPE",
+    specializedPPE: "Specialized PPE",
+    hsePersonnel: "HSE Personnel",
+    projectManager: "Project Manager",
+    projectControlSpecialist: "Project Control Specialist",
+    logisticsCoordinator: "Logistics Coordinator",
+    qualityControlSpecialist: "Quality Control Specialist",
+    itAndTelecomEngineers: "IT and Telecom Engineers",
+    mechanicalEngineers: "Mechanical Engineers",
+    mechanicalFitters: "Mechanical Fitters",
+    welders: "Welders",
+    electricalEngineers: "Electrical Engineers",
+    instrumentationEngineers: "Instrumentation Engineers",
+    maintenanceEngineers: "Maintenance Engineers",
+    products: "Products",
+    specializedPPE_2: "Specialized PPE",
+    ppe: "PPE",
+    hoseTubingAndFitting_2: "Hose Tubing and Fitting",
+    materialHandling_2: "Material Handling",
+    fastener: "Fastener",
+    mechanicalTools_2: "Mechanical Tools",
+    instrumentation_2: "Instrumentation",
+    telecommunication_2: "Telecommunication",
+    surgeArrestors_2: "Surge Arrestors",
+    valueproposition: "Value Proposition",
+
+    valueProposition1:
+      "INOVATEC offers Highly Customized and International Level Procurement and Supply of Timely Delivery of Specialized Equipment and Personnel in Angola.",
+    valueProposition2:
+      "INOVATEC has skilled engineers with extensive global sourcing experience and clear understanding of Local Import mechanisms.",
+    valueProposition3:
+      "INOVATEC has a rich database of skilled personnel (national and international) which we can call upon to attend the needs of our clients.",
+    valueProposition4:
+      "INOVATEC has practice from other local clients in deciphering client’s requirements through their stated goals.",
+    valueProposition5:
+      "Lead times are better managed by having stocking capabilities.",
+    valueProposition6:
+      "Collaboration with large stock holders of specialized equipment in the South African, European, Middle East, and Asian Market.",
+    valueProposition7:
+      "Our partner base allows us to strike a balance between cost-effectiveness and lead times, by working closely with reliable partners.",
+    valueProposition8:
+      "We also ensure quality and authentic products by working only with certified suppliers of any specialized equipment.",
+    valueProposition9:
+      "Highly skilled team to propose engineering solutions when needed, along with global expert partners offering alternative brands when possible.",
+    valuePropositionEnd:
+      "Global expert partners offering alternative brands when possible",
+    ourclients: "Our Clients",
+  },
+};
+
+let currentLanguage = "en";
+
+// Atualiza os textos no HTML
+function updateTexts() {
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const key = element.getAttribute("data-i18n");
+    const translation = translations[currentLanguage][key];
+    if (translation) {
+      element.textContent = translation;
+    } else {
+      console.warn(`Chave de tradução não encontrada: ${key}`);
+    }
+  });
+}
+
+// Muda o idioma ao selecionar no <select>
+function changeLanguage() {
+  const languageSelect = document.getElementById("languageSwitcher");
+  const selectedLanguage = languageSelect.value;
+
+  if (translations[selectedLanguage]) {
+    currentLanguage = selectedLanguage;
+    updateTexts();
+    const selectedOption = languageSelect.options[languageSelect.selectedIndex];
+    const imagePath = selectedOption.getAttribute("data-image");
+    languageSelect.style.backgroundImage = `url(${imagePath})`;
+  } else {
+    console.error(`Idioma não suportado: ${selectedLanguage}`);
+  }
+}
+
+// Inicializa o i18n com o idioma padrão
+document.addEventListener("DOMContentLoaded", () => {
+  updateTexts();
+});
